cd4007666072b76234011b06fe043ddbdff9ca0b
s1lentdragon <22919zpw@gmail.com>
2023-09-10 15:31:40 +0800

    add. init project

---
+++ src/main/java/com/sipc/hospital_alarm_system/HospitalAlarmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sipc.hospital_alarm_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAlarmSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalAlarmSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sipc/hospital_alarm_system/HospitalAlarmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipc.hospital_alarm_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAlarmSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
